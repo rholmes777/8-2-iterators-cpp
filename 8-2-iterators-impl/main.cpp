4968634b27e37a8eb864607d54854b348a77a3dd
Richard Holmes <rholmes@richardholmes.net>
2013-01-28 20:25:30 -0600

    Initial Commit

---
+++ 8-2-iterators-impl/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  8-2-iterators-impl
+//
+//  Created by Richard Holmes on 1/28/13.
+//  Copyright (c) 2013 Richard Holmes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
