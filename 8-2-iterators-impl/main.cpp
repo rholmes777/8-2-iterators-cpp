155cd8b7fb5d3771775ce01e365544d805256267
Richard Holmes <rholmes@richardholmes.net>
2013-02-23 12:52:27 -0600

    Standardize on deployment target 10.7 MacOS, LLVM C++ && stdc++ 11 library for both this project and the gtest project.  Has the Google Tests from the project.

--- 8-2-iterators-impl/main.cpp
+++ 8-2-iterators-impl/main.cpp
@@ -5,8 +5,29 @@
 //  Created by Richard Holmes on 1/28/13.
 //  Copyright (c) 2013 Richard Holmes. All rights reserved.
 //
+//  Implement our own versions of the following library algorithms,
+//  per exercise 8-2 in Accelerated C++ (Moo, Koenig)
+//      equal(b, e, b2)
+//      find(b, e, t)
+//      copy(b, e, d)
+//      remove_copy_if(b, e, d, p)
+//      transform(b, e, d f)
+//      accumulate(b, e, t)
+//      search(b, e, b2, e2)
+//      find_if(b, e, p)
+//      remove_copy(b, e, d, t)
+//      remove(b, e, t)
+//      partition(b, e, p)
+//
+
+
 
 #include <iostream>
+
+int dummy(void)
+{
+    return 2;
+}
 
 int main(int argc, const char * argv[])
 {
---
+++ Unit-GTests/sample1_unittest.h
@@ -0,0 +1,14 @@
+//
+//  sample1_unittest.h
+//  8-2-iterators-impl
+//
+//  Created by Richard Holmes on 2/19/13.
+//  Copyright (c) 2013 Richard Holmes. All rights reserved.
+//
+
+#ifndef ____2_iterators_impl__sample1_unittest__
+#define ____2_iterators_impl__sample1_unittest__
+
+#include <iostream>
+
+#endif /* defined(____2_iterators_impl__sample1_unittest__) */
