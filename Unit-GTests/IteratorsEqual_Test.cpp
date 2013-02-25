b45dc7cdb1f36093de72662ec616a00822c5167f
Richard Holmes <rholmes@richardholmes.net>
2013-02-24 20:47:02 -0600

    Remove the gtests from the google test build.  Use framework only.

---
+++ Unit-GTests/IteratorsEqual_Test.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unit-GTests
+//
+//  Created by Richard Holmes on 2/18/13.
+//  Copyright (c) 2013 Richard Holmes. All rights reserved.
+
+#include "gtest/gtest.h"
+
+
+TEST(SampleTest, TestPass)
+{
+    SUCCEED();
+}
